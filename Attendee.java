9737dd365e4881c12fd11613ab87fe91a9d43164
kailyn0215 <114490353+kailyn0215@users.noreply.github.com>
2023-11-17 10:45:09 -0500

    Update Attendee.java

--- Attendee.java
+++ Attendee.java
@@ -5,6 +5,10 @@ public class Attendee
 	private int id;
 	private String name;
 	private String company;
+	private String person;
+	
+	private String[] companies = {"Wal-Mart", "Kroger", "Amazon", "Lowes", "Best Western", "KMart", "Fusian", "Heinz", "Gucci", "Prada", "Nike", "Dodge", "Maserati", "Razor", "AMD", "Razer"};
+	private int[] compCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	
 	public Attendee(int initId, String initName, String initCompany)
 	{
@@ -12,6 +16,11 @@ public class Attendee
 		name = initName;
 		company = initCompany;
 	}
+	
+	/*public Attendee(String people)
+	{
+		person = people;
+	}*/
 	
 	public String toString()
 	{
@@ -33,12 +42,23 @@ public class Attendee
 		return company;
 	}
 	
-	public String split(String person)
+	public String companyName()
+	{
+		for (int x = 0; x < companies.length; x++)
+		{
+			if(Integer.parseInt(company) == x)
+			{
+				return companies[x];
+			}
+		}
+		return "ERROR";
+	}
+	
+	/*public void split()
 	{
 		String[] splitPerson = person.split(",", 5);
 		id = Integer.parseInt(splitPerson[1]);
 		name = splitPerson[3] + " " + splitPerson[2];
 		company = splitPerson[4];
-		return name;
-	}
+	}*/
 }
