76952c28189d0c655d9936275e2b06a4d3026389
kailyn0215 <114490353+kailyn0215@users.noreply.github.com>
2023-11-20 13:07:25 -0500

    Update Planner.java

--- Planner.java
+++ Planner.java
@@ -4,16 +4,21 @@ import java.util.*;
 
 public class Planner
 {
+	private int[] compCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	
 	// driver class
 	public static void main(String args[])
 	{
 		ArrayList<Attendee> guests = new ArrayList<Attendee>();
-		ArrayList<Attendee> tables = new ArrayList<Attendee>();
+		//ArrayList<Attendee> tables = new ArrayList<Attendee>();
+		String tables[][];
 		
 		int id;
 		String name;
 		String company;
 		int x = 0;
+		int maxTables = 10;
+		int maxPeople = 10;
 		
 		try 
 		{
@@ -23,12 +28,11 @@ public class Planner
 			{
 				//String person = myReader.nextLine();
 				String arr[] = myReader.nextLine().split(",");
-				
 				//uniqueid, last, first, compID
 				id = Integer.parseInt(arr[0]);
 				name = arr[2] + " " + arr[1];
-				company = arr[3];						
-				Attendee person = new Attendee(id, name, company);
+				company = arr[3];
+				//companyCount(company);						
 				guests.add(new Attendee(id, name, company));
 				System.out.print(guests.get(x));
 				x++;
@@ -41,8 +45,27 @@ public class Planner
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-		
+		for (int i = 0; i < maxTables; i++)
+		{
+			if(tables[i][j].equals("") && ) // need to add smth do check if the seat is free and if there is no one else from their company sitting there
+			{
+				
+			}
+		}
 	}
+	
+	private int comp;
+	
+	/*public boolean companyCount(String company)
+	{
+		comp = Integer.parseInt(company);
+		compCount[comp]++; // adds one count to the # of people from each company
+		if(compCount[comp] > 10)
+		{
+			return false;
+		}
+		return true;
+	}*/
 }
 /*
  * Attendee Info: 1 company rep per table - if more than 10 people from a company tell the user
