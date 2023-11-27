2063c9fc1e305c37369cac6325f2bfa2eb0c6a4d
kailyn0215 <114490353+kailyn0215@users.noreply.github.com>
2023-11-26 23:40:09 -0500

    Update Attendee.java

--- Attendee.java
+++ Attendee.java
@@ -3,28 +3,25 @@ import java.util.*;
 public class Attendee
 {
 	private int id;
-	private String name;
-	private String company;
+	private String firstName;
+	private String lastName;
+	private String companyName;
+	private int companyNumber;
 	private String person;
+	private int tableNumber;
 	
-	private String[] companies = {"Wal-Mart", "Kroger", "Amazon", "Lowes", "Best Western", "KMart", "Fusian", "Heinz", "Gucci", "Prada", "Nike", "Dodge", "Maserati", "Razor", "AMD", "Razer"};
-	private int[] compCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	
-	public Attendee(int initId, String initName, String initCompany)
+	public Attendee(int initId, String initFName, String initLName, String initCompany, int initCompNumb)
 	{
 		id = initId;
-		name = initName;
-		company = initCompany;			
+		firstName = initFName;
+		lastName = initLName;
+		companyName = initCompany;	
+		companyNumber = initCompNumb;		
 	}
-	
-	/*public Attendee(String people)
-	{
-		person = people;
-	}*/
 	
 	public String toString()
 	{
-		return ("\n\nID: " + id + ", Name: " + name + ", Company: " + company + "\n");
+		return ("\n\nID: " + id + ", Name: " + firstName + " " + lastName + ", Company: " + companyName + "\n");
 	}
 	
 	public int getId()
@@ -34,32 +31,26 @@ public class Attendee
 	
 	public String getName()
 	{
-		return name;
+		return firstName + " " + lastName;
 	}
 	
-	public String getCompany()
+	public int getCompanyNumber()
 	{
-		return company;
+		return companyNumber;
 	}
 	
-	public String companyName()
+	public String getCompanyName()
 	{
-		for (int x = 0; x < companies.length; x++)
-		{
-			if(Integer.parseInt(company) == x)
-			{
-				return companies[x];
-			}
-		}
-		return "ERROR";
+		return companyName;
 	}
 	
-	
-	/*public void split()
+	public void setTable(int num)
 	{
-		String[] splitPerson = person.split(",", 5);
-		id = Integer.parseInt(splitPerson[1]);
-		name = splitPerson[3] + " " + splitPerson[2];
-		company = splitPerson[4];
-	}*/
+		tableNumber = num;
+	}
+	
+	public int getTable()
+	{
+		return tableNumber;
+	}
 }
